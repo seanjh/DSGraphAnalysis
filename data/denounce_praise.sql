dfb43aeae985f1e00a129ea40ad0f3b48b1be94d
Sean Herman <seanherman@gmail.com>
2016-04-20 11:22:54 -0400

    updated query

--- data/denounce_praise.sql
+++ data/denounce_praise.sql
@@ -1,6 +1,6 @@
 SELECT
-  Actor1Code, Actor1Name, Actor1Type1Code,
-  Actor2Code, Actor2Name, Actor2Type1Code, Actor2Type2Code,
+  Actor1Code, Actor1Name, Actor1Type1Code, Actor1CountryCode,
+  Actor2Code, Actor2Name, Actor2Type1Code, Actor2CountryCode,
   MIN(Year) YearMin,
   MAX(Year) YearMax,
   EventCode,
@@ -12,7 +12,9 @@ WHERE
   Actor1Type1Code = 'GOV' AND
   (Actor2Type1Code = 'BUS' OR Actor2Type2Code = 'BUS') AND
   Actor1Name IS NOT NULL AND Actor2Name IS NOT NULL AND
+  Actor1CountryCode IS NOT NULL AND
+  Actor2CountryCode IS NOT NULL AND
   Actor1Code != Actor2Code
 GROUP BY
   Actor1Code, Actor1Name, Actor1Type1Code, Actor2Code, Actor2Name,
-  Actor2Type1Code, Actor2Type2Code, EventCode
+  Actor2Type1Code, Actor2Type2Code, EventCode, Actor1CountryCode, Actor2CountryCode
